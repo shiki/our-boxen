06f1f9736dc8a9a7c4a36e0bdc4e3eed4e3dc310
Shiki <shikishiji@gmail.com>
2013-03-21 20:07:41 +0800

    Web team: Sequel Pro. Shiki: Removed Pomodoro cause it sucks now.

--- modules/fufulabs_web/manifests/init.pp
+++ modules/fufulabs_web/manifests/init.pp
@@ -6,4 +6,9 @@ class fufulabs_web {
   include memcached
   include redis
   include mysql
+
+  package { 'Sequel_Pro':
+    provider => 'appdmg',
+    source => 'http://sequel-pro.googlecode.com/files/sequel-pro-1.0.1.dmg'
+  }
 }
--- modules/people/manifests/shiki.pp
+++ modules/people/manifests/shiki.pp
@@ -8,10 +8,5 @@ class people::shiki {
   include handbrake
   #include vlc
   include flux
-  
-  package { 'Pomodoro':
-    provider => 'appdmg',
-    source   => 'http://www.pomodoroapp.com/d/PomodoroApp.3.0.beta.dmg'
-  }
 }
 
